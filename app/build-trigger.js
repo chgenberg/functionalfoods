ff3331ced90b119dac8ecace0739239ce3a39628
Christopher Genberg <christopher@1753skincare.com>
2025-09-22 14:26:29 +0200

    trigger: Force rebuild due to slow Railway deploy

--- app/build-trigger.js
+++ app/build-trigger.js
@@ -1,3 +1,5 @@
-// Build trigger - updated at 2025-09-22 13:45
+// Build trigger - updated at 2025-09-22 14:25
 // This file is used to trigger new Railway deployments
+// Force rebuild due to slow initialization
 console.log('Build triggered at:', new Date().toISOString());
+console.log('Deployment status: Force rebuild initiated');
