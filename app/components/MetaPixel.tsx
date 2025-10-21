e9dbe9989ca11266f158bcdccdb22d5eab1a60ab
Christopher Genberg <christopher@1753skincare.com>
2025-10-22 01:09:35 +0200

    Fix META Pixel loading - use official FB loader with beforeInteractive strategy

--- app/components/MetaPixel.tsx
+++ app/components/MetaPixel.tsx
@@ -75,31 +75,20 @@ export default function MetaPixel() {
 
   return (
     <>
-      {/* Default to revoke until consent banner grants marketing */}
-      <Script id="fbp-consent-default" strategy="afterInteractive">
+      {/* Initialize fbq stub and load script inline for better reliability */}
+      <Script id="fbp-init" strategy="beforeInteractive">
         {`
-          window.fbq = window.fbq || function(){(window.fbq.q=window.fbq.q||[]).push(arguments)};
-          window._fbq = window._fbq || window.fbq;
-          window.fbq('consent','revoke');
-        `}
-      </Script>
-      <Script
-        id="fbp-loader"
-        strategy="afterInteractive"
-        src="https://connect.facebook.net/en_US/fbevents.js"
-        onLoad={() => {
-          try {
-            updateMarketingConsentFromStorage();
-            ensureInitialPixelPageViewOnce();
-          } catch {}
-        }}
-      />
-      <Script id="fbp-init" strategy="afterInteractive">
-        {`
-          if (window.fbq) {
-            fbq('init', '${PIXEL_ID}');
-            fbq('track', 'PageView');
-          }
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('consent', 'revoke');
+          fbq('init', '${PIXEL_ID}');
+          fbq('track', 'PageView');
         `}
       </Script>
       {/* NoScript fallback */}
