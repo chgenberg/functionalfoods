5fb893cbc38c9236b1307765be1a661d0445ae95
Christopher Genberg <christopher@genberg.se>
2025-10-27 18:14:17 +0100

    Add remaining week 3 recipes: curry, scampi, torsk, pizza

--- app/api/admin/seed/hormone/week3/recipes/route.ts
+++ app/api/admin/seed/hormone/week3/recipes/route.ts
@@ -92,6 +92,95 @@ const RECIPES: RecipeSeed[] = [
       'Toppa med falafel och dressing.',
       'Dekorera med en persiljekvist.'
     ]
+  },
+  {
+    title: 'Kyckling med grön curry',
+    servings: 2,
+    image: `${IMG_BASE}/KYCKLING_MED_GRÖN_CURRY.JPG`,
+    categories: ['kyckling','middag'],
+    ingredients: ['300 g kycklinglårfilé','1/2 vitlöksklyfta','1/2 msk ingefära','1 tsk ketjap manis','Salt och svartpeppar','1/2 vitlögsklyfta','1/2 msk ingefära','1 morot','1 salladslök','200 ml kokosmjölk','20 g green curry kryddmix','1 tsk smör','100 g sockerärtor','1/2 dl cashewnötter','1/2 dl salladslök','1 msk färsk koriander'],
+    instructions: [
+      'Skär kycklinglårfilé i mindre bitar.',
+      'Lägg i en skål.',
+      'Skala och riv vitlök och ingefära och blanda ner i skålen tillsammans med ketjap manis, salt och peppar.',
+      'Låt kycklingen marinera i 15 minuter.',
+      'Skala och riv vitlök och ingefära.',
+      'Skala och skär morot i skivor.',
+      'Häll kokosmjölk i en kastrull.',
+      'Tillsätt green curry, vitlök och ingefära och låt koka upp.',
+      'Hetta upp en stekpanna med smör.',
+      'Stek kycklinglårfilé hastigt.',
+      'Lägg över kycklingen i kastrullen med kokosmjölk.',
+      'Låt koka i 5 minuter.',
+      'Tillsätt morötter efter några minuter.',
+      'Strimla sockerärtor och salladslök och lägg ner i grytan.',
+      'Koka ytterligare i en minut.',
+      'Hacka koriander och strimla salladslök.',
+      'Lägg upp på en tallrik och dekorera med färsk koriander, salladslök och cashewnötter.'
+    ]
+  },
+  {
+    title: 'Scampi med mangosallad',
+    servings: 2,
+    image: `${IMG_BASE}/SCAMPI_MED_MANGOSALLAD.JPG`,
+    categories: ['fisk','middag'],
+    ingredients: ['250 g fryst scampi','1/2 mango','1/4 paprika','1/2 salladslök','2 msk mango chutney','1/2 dl grekisk yoghurt','Salt och svartpeppar','2 msk koriander','2 hjärtsalladshuvuden','1 tsk olivolja','1/2 vitlögsklyfta','1/2 röd chili','2 msk salladslök','2 msk röd paprika','1/2 mango','2 limeklyftor'],
+    instructions: [
+      'Tina scampi.',
+      'Skala och skär mango i tärningar.',
+      'Finhacka paprika och strimla salladslök.',
+      'Hacka koriander.',
+      'Lägg i en skål och blanda ner mango chutney och grekisk yoghurt.',
+      'Salta och peppra.',
+      'Lägg upp hjärtsalladsblad på ett fat.',
+      'Fördela mangoröran över.',
+      'Skala och riv vitlök och skär chili i tunna skivor.',
+      'Hetta upp en stekpanna med olja och fräs scampi, vitlök och chili några minuter.',
+      'Strimla salladslök.',
+      'Finhacka paprika och skär mango i skivor.',
+      'Fördela scampi på salladen och strö på salladslök och paprika.',
+      'Dekorera med skivad mango och limeklyftor.'
+    ]
+  },
+  {
+    title: 'Torskgratäng med champinjoner',
+    servings: 4,
+    image: `${IMG_BASE}/TORSKGRATÄNG_MED_CHAMPINJONER.JPG`,
+    categories: ['fisk','middag'],
+    ingredients: ['300 g champinjoner','250 g cocktailtomater','2 tomater','10 cm squash','3/4 rödlök','1 vitlöksklyfta','1 msk olivolja','Salt och svartpeppar','1 tsk örtagårdskrydda','700 g torskrygg','2 msk sweet chilisås','1/2 dl jalapeños','1 dl riven ost','4 persiljekvistar'],
+    instructions: [
+      'Sätt ugnen på 175 grader.',
+      'Lägg hela champinjoner och cocktailtomater i en ugnsfast form.',
+      'Skär tomater i klyftor och skiva squash.',
+      'Skala och skiva lök och riv vitlök.',
+      'Blanda ner i formen tillsammans med olivolja, salt, peppar och örtagårdskrydda.',
+      'Strö på salt, peppar och örtagårdskrydda på torsken.',
+      'Lägg torsken på grönsakerna och häll över sweet chilisås.',
+      'Strö på riven ost och toppa med skivade jalapeños.',
+      'Ugnsbaka i 15 minuter.',
+      'Dekorera med en persiljekvist.'
+    ]
+  },
+  {
+    title: 'Italiensk pizza med skinka',
+    servings: 2,
+    image: `${IMG_BASE}/ITALIENSK_PIZZA_MED_SKINKA.JPG`,
+    categories: ['pizza','middag'],
+    ingredients: ['2 ägg','1 msk fiberhusk','1 tsk bakpulver','125 g riven ost','4 msk tomatsås färdig','125 g mozzarella','3 champinjoner','1 krm torkad oregano','75 g lufttorkad skinka','1/4 rödlök','4 cocktailtomater','1 msk färsk basilika'],
+    instructions: [
+      'Sätt ugnen på 200 grader.',
+      'Blanda alla ingredienser till degen och låt svälla i 5 minuter.',
+      'Lägg ett bakplåtspapper på en plåt och klicka ut en eller två pizzor av degen.',
+      'Platta till och förgrädda i cirka 8 minuter.',
+      'Ta ut pizzan och bred på ett jämnt lager tomatsås.',
+      'Skär mozzarella i skivor och fördela ost över.',
+      'Skiva champinjoner och lägg över och strö på oregano.',
+      'Grädda i ugnen tills osten blivit gyllenbrun, cirka 7-8 minuter.',
+      'Skala och skär rödlök i strimlor.',
+      'Dela tomaterna.',
+      'Fördela lök, tomat och lufttorkad skinka på pizzan/pizzorna.',
+      'Dekorera med basilikablad.'
+    ]
   }
 ];
 
