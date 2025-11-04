6ddbbe5abadeeb496aa66cc7024166b38caf2851
Christopher Genberg <christopher@genberg.se>
2025-11-04 14:57:54 +0100

    Lägg till type declaration för PDFKit standalone

--- types/pdfkit.d.ts
+++ types/pdfkit.d.ts
@@ -1,4 +1,9 @@
 declare module 'pdfkit' {
   const PDFDocument: any;
   export default PDFDocument;
+}
+
+declare module 'pdfkit/js/pdfkit.standalone.js' {
+  const PDFDocument: any;
+  export default PDFDocument;
 } 
