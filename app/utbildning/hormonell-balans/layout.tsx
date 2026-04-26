35fb804a4bb62225a71cff70da78f6b7d411abe4
christos-ballesteros <166001233+christos-ballesteros@users.noreply.github.com>
2026-04-26 19:57:56 +0200

    Update image path for Hormonell Balans layout

--- app/utbildning/hormonell-balans/layout.tsx
+++ app/utbildning/hormonell-balans/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = generateSEOMetadata({
     'kurs',
   ],
   url: '/utbildning/hormonell-balans',
-  image: '/Hormonell_balans/hormonell_balans_kurssida.png',
+  image: '/public/Hormonell_balans/hormonell_balans_kurssida.png',
   type: 'website',
 });
 
